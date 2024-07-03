d4193599636bb254e74ac91080ceba5ca3dcd1fe
John Lloyd Centeno <johnloyd.centeno@gmail.com>
2024-07-03 20:53:03 +0800

    Responsive Fix

--- src/pages/homepage/homePersuasion/HomePersuasion.tsx
+++ src/pages/homepage/homePersuasion/HomePersuasion.tsx
@@ -1,34 +1,106 @@
-import { Box, Group, Paper, Image } from "@mantine/core";
+import {
+  Box,
+  Group,
+  Paper,
+  Image,
+  Stack,
+  AspectRatio,
+  Text,
+  Center,
+} from "@mantine/core";
 import "./HomePersuasion.css";
-import getPaddingResponsive from "../../../utils/getPaddingResponsive";
+import AkbayanLogo from "./overlay/akbayanLogo/AkbayanLogo";
 
 function HomePersuasion() {
-  const paddingResponsive = getPaddingResponsive();
-
   return (
-    <Paper id="home-persuasion" component="section" radius={0}>
-      <Group px={paddingResponsive}>
+    <Paper id="home-persuasion" component="section" radius={0} pos="relative">
+      <Group>
         <Box component="div" className="persuation-grid">
-          <Box className="persuation-item item-1">
-            <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" />
-          </Box>
-          <Box className="persuation-item item-2">
-            <Image
-              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
-              h="100%"
-            />
-          </Box>
-          <Box className="persuation-item item-3">
-            <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" />
-          </Box>
-          <Box className="persuation-item item-4">
-            <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" />
-          </Box>
-          <Box className="persuation-item item-5">
-            <Image src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png" />
-          </Box>
+          <Stack className="persuation-grid__item__stack">
+            <Box pos="relative" className="persuation-item">
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src="./public/homepage/Organizations/Gabriela/Grabiela.jpg"
+                  h="100%"
+                />
+              </AspectRatio>
+              <Box
+                component="div"
+                className="persuation-item__overlay"
+                pos="absolute"
+              >
+                <Image
+                  display="inline-block"
+                  height="50%"
+                  src="./public/homepage/Organizations/Gabriela/Gabriela_Overlay.webp"
+                />
+              </Box>
+            </Box>
+
+            <Box pos="relative" className="persuation-item">
+              <AspectRatio ratio={16 / 9}>
+                <Image src="./public/homepage/Organizations/Girlup/girlup.png" />
+              </AspectRatio>
+              <Box
+                component="div"
+                className="persuation-item__overlay"
+                pos="absolute"
+              ></Box>
+            </Box>
+          </Stack>
+
+          <Stack>
+            <Box className="persuation-item middle" h="100%">
+              <Image
+                h="100%"
+                src="./public/homepage/Organizations/middle.jpg"
+              />
+            </Box>
+          </Stack>
+
+          <Stack className="persuation-grid__item__stack">
+            <Box pos="relative" className="persuation-item">
+              <AspectRatio ratio={16 / 9}>
+                <Image src="./public/homepage/Organizations/HRW/hrw.webp" />
+              </AspectRatio>
+              <Box
+                component="div"
+                className="persuation-item__overlay"
+                pos="absolute"
+              >
+                <Image
+                  display="inline-block"
+                  height="60%"
+                  src="./public/homepage/Organizations/HRW/hrw_overlay.jpg"
+                />
+              </Box>
+            </Box>
+
+            <Box pos="relative" className="persuation-item">
+              <AspectRatio ratio={16 / 9}>
+                <Image src="./public/homepage/Organizations/Akbayan/Akbayan (2).jpg" />
+              </AspectRatio>
+              <Box
+                component="div"
+                className="persuation-item__overlay"
+                pos="absolute"
+              >
+                <AkbayanLogo />
+              </Box>
+            </Box>
+          </Stack>
         </Box>
       </Group>
+      <Stack gap={0} pos="absolute" className="home-persuasion__main-text">
+        <Text tt="uppercase" className="title" fw={700}>
+          Join The Movement
+        </Text>
+        <Center>
+          <Text className="content">
+            Choose The Organization That Truly Acts
+          </Text>
+        </Center>
+      </Stack>
     </Paper>
   );
 }
