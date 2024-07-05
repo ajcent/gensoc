affb34de43a57bcc184fd0e41d55105d3c384d22
John Lloyd Centeno <johnloyd.centeno@gmail.com>
2024-07-05 18:22:48 +0800

    V1.1 Release

--- src/pages/homepage/homeSlide/HomeSlide.tsx
+++ src/pages/homepage/homeSlide/HomeSlide.tsx
@@ -2,7 +2,7 @@ import "./HomeSlide.css";
 import "@mantine/carousel/styles.css";
 
 import { Carousel } from "@mantine/carousel";
-import { Center, Text } from "@mantine/core";
+import { BackgroundImage, Center, Text } from "@mantine/core";
 
 import { useRef } from "react";
 import Autoplay from "embla-carousel-autoplay";
@@ -27,40 +27,61 @@ function HomeSlide() {
           className="hero-carousel no-select"
         >
           <Carousel.Slide>
-            <Center h="100%">
-              <Text
-                className="carousel-text"
-                px={{ md: "5rem", base: "3rem" }}
-                ta="center"
-              >
-                Your gender influence your experience at crisis, healthcare,
-                relationship, politics, and more. It encompass everything.
-              </Text>
-            </Center>
+            <BackgroundImage
+              h="100%"
+              src="https://images.unsplash.com/photo-1581078426770-6d336e5de7bf?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            >
+              <Center className="carousel-center" h="100%">
+                <Text
+                  className="carousel-text"
+                  px={{ md: "5rem", base: "3rem" }}
+                  ta="center"
+                  fw={600}
+                >
+                  Your gender influence your experience at crisis, healthcare,
+                  relationship, politics, and more. It encompass everything.
+                </Text>
+              </Center>
+            </BackgroundImage>
           </Carousel.Slide>
+
           <Carousel.Slide>
-            <Center h="100%">
-              <Text
-                className="carousel-text"
-                px={{ md: "5rem", base: "3rem" }}
-                ta="center"
-              >
-                Gender inequality disproportionately affects women and girls.
-                This is more seen to the minority parts of the gender spectrum.
-              </Text>
-            </Center>
+            <BackgroundImage
+              h="100%"
+              src="https://images.unsplash.com/photo-1476469535352-80159d0af31d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            >
+              <Center className="carousel-center" h="100%">
+                <Text
+                  className="carousel-text"
+                  px={{ md: "5rem", base: "3rem" }}
+                  ta="center"
+                  fw={600}
+                >
+                  Gender inequality disproportionately affects women and girls.
+                  This is more seen to the minority parts of the gender
+                  spectrum.
+                </Text>
+              </Center>
+            </BackgroundImage>
           </Carousel.Slide>
+
           <Carousel.Slide>
-            <Center h="100%">
-              <Text
-                className="carousel-text"
-                px={{ md: "5rem", base: "3rem" }}
-                ta="center"
-              >
-                In most societies, they have lower status and have less control
-                over decision-making about their life.
-              </Text>
-            </Center>
+            <BackgroundImage
+              h="100%"
+              src="https://images.unsplash.com/photo-1594230612535-e4a08d4cb18b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            >
+              <Center className="carousel-center" h="100%">
+                <Text
+                  className="carousel-text"
+                  px={{ md: "5rem", base: "3rem" }}
+                  ta="center"
+                  fw={600}
+                >
+                  In most societies, they have lower status and have less
+                  control over decision-making about their life.
+                </Text>
+              </Center>
+            </BackgroundImage>
           </Carousel.Slide>
         </Carousel>
       </div>
